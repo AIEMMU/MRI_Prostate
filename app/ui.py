5aa2b90a975f60312c1a83b18accceef819b6c21
AIEMMU <49679389+AIEMMU@users.noreply.github.com>
2021-01-07 15:56:56 +0000

    change for Pyinstaller

--- app/ui.py
+++ app/ui.py
@@ -13,15 +13,16 @@
 # from viewer import Viewer
 #
 # import cv2
-import torchvision
-def script_method(fn, _rcb=None):
-    return fn
-def script(obj, optimize=True, _frames_up=0, _rcb=None):
-    return obj
-import torch.jit
-torch.jit.script_method = script_method
-torch.jit.script = script
-
+# import torchvision
+# def script_method(fn, _rcb=None):
+#     return fn
+# def script(obj, optimize=True, _frames_up=0, _rcb=None):
+#     return obj
+# import torch.jit
+# torch.jit.script_method = script_method
+# torch.jit.script = script
+import os
+os.environ["PYTORCH_JIT"] = "0"
 from PyQt5 import QtWidgets, QtGui, QtCore
 from displayView import MainWindow
 from displayViewModel import *
