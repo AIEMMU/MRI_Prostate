51e50fef4ab530c3d8b8e6d8244fbcc4f1430fc5
AIEMMU <49679389+AIEMMU@users.noreply.github.com>
2021-01-07 16:01:29 +0000

    Add files via upload

--- app/test.py
+++ app/test.py
@@ -1,34 +1,26 @@
-import sys, math
-from PyQt5 import QtCore, QtGui, QtWidgets
+import torchvision
 
-class MyWidget(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        QtWidgets.QWidget.__init__(self, parent)
-        self.pen = QtGui.QPen(QtGui.QColor(0,0,0))                      # set lineColor
-        self.pen.setWidth(3)                                            # set lineWidth
-        self.brush = QtGui.QBrush(QtGui.QColor(255,255,255,255))        # set fillColor  
-        self.polygon = self.createPoly(8,150,0)                         # polygon with n points, radius, angle of the first point
 
-    def createPoly(self, n, r, s):
-        polygon = QtGui.QPolygonF() 
-        w = 360/n                                                       # angle per step
-        for i in range(n):                                              # add the points of polygon
-            t = w*i + s
-            x = r*math.cos(math.radians(t))
-            y = r*math.sin(math.radians(t))
-            polygon.append(QtCore.QPointF(self.width()/2 +x, self.height()/2 + y))  
+def script_method(fn, _rcb=None):
+    return fn
 
-        return polygon
 
-    def paintEvent(self, event):
-        painter = QtGui.QPainter(self)
-        painter.setPen(self.pen)
-        painter.setBrush(self.brush)  
-        painter.drawPolygon(self.polygon)
+def script(obj, optimize=True, _frames_up=0, _rcb=None):
+    return obj
 
-app = QtWidgets.QApplication(sys.argv) 
 
-widget = MyWidget()
-widget.show()
+import torch.jit
 
-sys.exit(app.exec_())
+torch.jit.script_method = script_method
+torch.jit.script = script
+
+from PyQt5 import QtWidgets, QtGui, QtCore
+from displayView import MainWindow
+from displayViewModel import *
+from dicom import dicom
+from predictor import predictor
+from utils import getPixmap, enableButtons
+import torch
+
+a = torch.tensor((1,2))
+print(a)
